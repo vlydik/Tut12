b5e6173bb8788673269094155fb448c543691342
Vladyslav Kutsenko <s19183@pjwstk.edu.pl>
2020-06-21 21:33:11 +0300

    Add project files.

---
+++ Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Tut12.Models
+{
+    public class Album
+    {
+        public int IdAlbum { get; set; }
+        public string AlbumName { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string IdMusicLabel { get; set; }
+        public virtual MusicLabel MusicLabel { get; set; }
+        public virtual ICollection<Track> Tracks { get; set; }
+    }
+}
---
+++ Models/MusicLabel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Tut12.Models
+{
+    public class MusicLabel
+    {
+        public int IdMusicLabel { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Album> Albums { get; set; }
+
+    }
+}
